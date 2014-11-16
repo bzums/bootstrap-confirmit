bd631c63ec5f3623cc0f2be59e2158679398c9f8
bzums <bsieker@mail.upb.de>
2014-11-16 17:20:00 +0100

    0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 bootstrap-confirmit
-===================
+========
+A [Bootstrap](https://github.com/twbs/bootstrap) component for easy inline confirmation buttonsets to avoid (modal) dialogs to confirm a specific action. Utilizes [Animate.css](https://github.com/daneden/animate.css) (optional) to add animations.
 
-Inline UI confirmation widget for Bootstrap to avoid dialogs etc.
+
+## Dependenices
+Bootstrap 3 only.
+
+## Examples
+Download or clone repository and run ```bower install```. Then open index.html.
+
+## Usage
+Stay tuned.
+
+## Tests
+Stay tuned.
---
+++ src/bootstrap-confirmit.js
@@ -0,0 +1,245 @@
+(function($) {
+    'use strict';
+
+    // CONFIRMIT CLASS DEFINITION
+    // =========================
+
+    var Confirmit = function(element, options) {
+
+        this.init('confirmit', element, options);
+    };
+
+    Confirmit.DEFAULTS = {
+        confirmBtnClassOnShow: 'fadeInLeft animated',
+        confirmBtnClassOnHide: 'fadeOutLeft animated',
+        autoClose: true,
+        autoCloseAfter: 2000, 
+        onShow: function (e) {},
+        onHide: function (e) {},
+        onConfirm: function (e) {}
+    };
+
+    Confirmit.prototype.init = function(type, element, options) {
+
+        this.type = type;
+        this.element = element;
+        this.$element = $(element).on('click.bs.confirmit', $.proxy(this.toggle, this));
+        this.options = this.getOptions(options);
+
+        this.autoCloseTimer = false;
+
+        var $parent = getParent(this.$element);
+        $parent.find('.confirmit-confirm').on('click', this.options.onConfirm);
+    }
+
+    Confirmit.prototype.getDefaults = function() {
+
+        return Confirmit.DEFAULTS;
+    }
+
+    Confirmit.prototype.getOptions = function(options) {
+
+        options = $.extend({}, this.getDefaults(), this.$element.data(), options);
+        return options;
+    }
+
+    Confirmit.prototype.toggle = function() {
+
+        console.log('toggle');
+
+        var $parent = getParent(this.$element);
+        var isActive = $parent.hasClass('open');
+
+        if (!isActive) {
+
+            return this.show();
+        } else {
+
+            return this.hide();
+        }
+    }
+
+    Confirmit.prototype.show = function() {
+
+        console.log('show');
+
+        var self = this;
+
+        if (self.$element.is('.disabled, :disabled')) return;
+
+        var $parent = getParent(self.$element);
+        var isActive = $parent.hasClass('open');
+
+        if (!isActive) {
+
+            var relatedTarget = {
+                relatedTarget: self.element
+            };
+
+            var e = $.Event('show.bs.confirmit', relatedTarget);
+            
+            $parent.trigger(e);
+            self.options.onShow.call(self.element, e);
+
+            if (e.isDefaultPrevented()) return;
+
+            $parent
+                .toggleClass('open')
+                .trigger('shown.bs.confirmit', relatedTarget);
+
+            var $confirm = $parent.find('.confirmit-confirm');
+            $confirm
+                .removeClass(self.options.confirmBtnClassOnHide)
+                .addClass(self.options.confirmBtnClassOnShow)
+                .show();
+
+            if ($confirm.hasClass('animated')) {
+
+                // When the animation is over, remove coresponding classes.
+                $confirm.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+                    function () {
+
+                        // Check, whether the button state has changed while the animation.
+                        if ($parent.hasClass('open')) {
+
+                            $confirm.removeClass(self.options.confirmBtnClassOnShow);
+                        }
+                });
+
+            }            
+
+            self.$element.focus();
+
+            $(document).one('click.bs.confirmit.data-api', $.proxy(self.hide, self));
+
+            if (self.options.autoClose === true) {
+            
+                self.autoCloseTimer = setTimeout($.proxy(self.hide, self), self.options.autoCloseAfter);
+            }
+        }
+
+        return false;
+    }
+
+    Confirmit.prototype.hide = function() {
+
+        console.log('hide');
+
+        var self = this;
+
+        if (self.$element.is('.disabled, :disabled')) return;
+
+        var $parent = getParent(self.$element);
+        var isActive = $parent.hasClass('open');
+
+        if (isActive) {
+
+            if (!$parent.hasClass('open')) return;
+
+            var relatedTarget = {
+                relatedTarget: self.element
+            };
+
+            var e = $.Event('hide.bs.confirmit', relatedTarget);
+            
+            $parent.trigger(e);
+            self.options.onHide.call(self.element, e);
+
+            if (e.isDefaultPrevented()) return;
+
+            $parent.removeClass('open').trigger('hidden.bs.confirmit', relatedTarget);
+
+            var $confirm = $parent.find('.confirmit-confirm');
+
+            $confirm.removeClass(self.options.confirmBtnClassOnShow)
+                .addClass(self.options.confirmBtnClassOnHide);
+
+            if ($confirm.hasClass('animated')) {
+
+                // When the animation is over, remove coresponding classes.
+                $confirm.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+                    function () {
+
+                        // Check, whether the button state has changed while the animation.
+                        if (!$parent.hasClass('open')) {
+
+                            $confirm.removeClass(self.options.confirmBtnClassOnHide)
+                                .hide();
+                        }
+                });
+
+            } else {
+
+                // If the element is not animated, just hide it.
+                $confirm.hide();
+            } 
+
+            self.$element.blur();
+
+            if (self.autoCloseTimer !== false){
+
+                clearTimeout(self.autoCloseTimer);
+                self.autoCloseTimer = false;
+            }
+        }
+
+        return false;
+    }
+
+    function getParent($elem) {
+
+        var selector = $elem.attr('data-target')
+
+        var $parent = selector && $(selector);
+
+        return ($parent && $parent.length) ? $parent : $elem.parent();
+    }
+
+
+    // CONFIRMIT PLUGIN DEFINITION
+    // =========================
+    function Plugin(option) {
+
+        return this.each(function() {
+
+            var $this = $(this);
+
+            var data = $this.data('bs.confirmit');
+            var options = typeof option == 'object' && option;
+            var selector = options && options.selector;
+
+            if (!data && option == 'destroy') return;
+
+            if (selector) {
+
+                if (!data) $this.data('bs.confirmit', (data = {}));
+                if (!data[selector]) data[selector] = new Confirmit(this, options);
+
+            } else {
+
+                if (!data) $this.data('bs.confirmit', (data = new Confirmit(this, options)));
+            }
+            if (typeof option == 'string') data[option]();
+        });
+    }
+
+    var old = $.fn.confirmit;
+
+    $.fn.confirmit = Plugin;
+    $.fn.confirmit.Constructor = Confirmit;
+
+    // CONFIRMIT PLUGIN DEFINITION
+    // ==========================
+
+    var old = $.fn.confirmit;
+
+    // CONFIRMIT NO CONFLICT
+    // ====================
+
+    $.fn.confirmit.noConflict = function() {
+
+        $.fn.confirmit = old;
+        return this;
+    }
+
+})(jQuery);
